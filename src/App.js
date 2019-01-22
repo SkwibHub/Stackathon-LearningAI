b1600236ba44fc8525b2d0cadf34f598a6c7c275
SkwibHub <eidolongroups@gmail.com>
2019-01-21 17:32:41 -0800

    'Fix: Minor interface fixes.'

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <p> My really bad idea for a Stackathon project goes here.</p>
-        <br />
+        <h1>My First Machine Learning Experiment</h1>
+        <p>
+          {' '}
+          Attempting machine learning with Tic-Tac-Toe. At the start, the
+          computer has no experience with Tic-Tac-Toe. The EXPLORE policy forces
+          it to explore moves it hasn't tried before or has tried the least. The
+          COMPETE policy has it play the best moves that it knows from
+          experience. After a while, the computer should only get draws, as the
+          best moves by both players lead to drawn games.
+        </p>
+        <p />
         <GameProcess />
       </div>
     );
